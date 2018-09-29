7153928742fa3620e05c596b359e3bd61d36060b
adhidevara <43514620+adhidevara@users.noreply.github.com>
2018-09-29 22:52:30 +0700

    Update formawal.php
    ARIEL ADHIDEVARA
    6701174117
    D3MI-41-04

--- formawal.php
+++ formawal.php
@@ -1,5 +1,24 @@
+<form method="POST">
+	<h1>LOGIN~</h1>
+	<table>
+		<tr>
+			<td>USERNAME</td>
+			<td>:</td>
+			<td><input type="text" name="usr"></td>
+		</tr>
+		<tr>
+			<td>PASSWORD</td>
+			<td>:</td>
+			<td><input type="password" name="pass"></td>
+		</tr>
+		<tr>
+			<td></td>
+			<td></td>
+			<td align="left"><input type="submit" name="submit" value="Kirim"></td>
+		</tr>
+	</table>
+</form>
+
 <?php
-
-
-
+	include 'proses.php';
 ?>

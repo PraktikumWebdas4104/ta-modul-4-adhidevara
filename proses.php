a44f98f1f6360abed5b6a6a47a07a4288742c003
adhidevara <43514620+adhidevara@users.noreply.github.com>
2018-09-29 22:53:38 +0700

    Update proses.php
    ARIEL ADHIDEVARA
    6701174117
    D3MI-41-04

--- proses.php
+++ proses.php
@@ -1,4 +1,65 @@
 <?php
+if (isset($_POST['submit'])) {
+		$usr = $_POST['usr'];
+		$pass = $_POST['pass'];
 
+		$admin = array('usr'  => 'admin',
+					   'pass' => 'admin123');
 
+		if ($usr==$admin['usr'] && $pass==$admin['pass']) {
+			echo "<h1>LOGIN SUKSES</h1> Hai, <b>".$usr."</b> Selamat Datang!~<br>";
+?>
+<form method="POST" align='center'>
+	<br><br><b>Pilih Genre Film Favorit :</b><br><br>
+	<table align="center">
+		<tr>
+			<td><input type="checkbox" name="film[]" value="Horror">Horror</td>
+			<td><input type="checkbox" name="film[]" value="Action">Action</td>
+		</tr>
+		<tr>
+			<td><input type="checkbox" name="film[]" value="Anime">Anime</td>
+			<td><input type="checkbox" name="film[]" value="Thriller">Thriller</td>
+		</tr>
+		<tr>
+			<td><input type="checkbox" name="film[]" value="Animasi">Animasi</td>
+		</tr>
+	</table>
+
+	<br><br><b>Pilih Tujuan Wisata Favorit Travelling :</b><br><br>
+	<table align="center">
+		<tr>
+			<td><input type="checkbox" name="travel[]" value="Bali">Bali</td>
+			<td><input type="checkbox" name="travel[]" value="Raja Ampat">Raja Ampat</td>
+		</tr>
+		<tr>
+			<td><input type="checkbox" name="travel[]" value="Pulau Derawan">Pulau Derawan</td>
+			<td><input type="checkbox" name="travel[]" value="Bangka Belitung">Bangka Belitung</td>
+		</tr>
+		<tr>
+			<td><input type="checkbox" name="travel[]" value="Labuan Bajo">Labuan Bajo</td>
+		</tr>
+	</table>
+	<br><input type="submit" name="submit2" value="Kirim">
+</form>
+
+<?php
+		}
+		else{
+			echo "<h1>USERNAME / PASSWORD SALAH";
+		}
+	}
+	if (isset($_POST['submit2'])) {
+			$dataFilm = $_POST['film'];
+			$dataTravel = $_POST['travel'];
+
+			echo "<b>Film Favorit Anda :</b><br>";
+			foreach ($dataFilm as $film) {
+				echo "◄. ".$film."<br>";
+			}
+
+			echo "<br><b>Tujuan Travelling Favorit Anda :</b><br>";
+			foreach ($dataTravel as $travel) {
+				echo "◄. ".$travel."<br>";
+			}
+	}
 ?>
